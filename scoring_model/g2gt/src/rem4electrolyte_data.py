b6344c3e760716599156239cc55d4b6cc52d35a2
ddd <542260219@qq.com>
2024-01-30 17:14:16 +0800

    g2g add model merging

--- scoring_model/g2gt/src/rem4electrolyte_data.py
+++ scoring_model/g2gt/src/rem4electrolyte_data.py
@@ -323,15 +323,15 @@ class Rem():
 
 
             trainer.fit(model=self.model, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader )
-            model_path="lightning_logs/%s/model_%s.ckpt" % (self.args.log_name,fold_num)
+            model_path="lightning_logs/%s/model_%s.ckpt" % (self.args.log_name,fold)
             trainer.save_checkpoint(model_path)
 
 
-            self.model.test_outputs_csv_path = "lightning_logs/%s/test_output_iid_%s.csv" % (self.args.log_name, fold_num)
+            self.model.test_outputs_csv_path = "lightning_logs/%s/test_output_iid_%s.csv" % (self.args.log_name, fold)
             trainer.test(model=self.model, dataloaders=iid_test_dataloader,ckpt_path=model_path)
             test_outputs_iid_csv_path_list.append(self.model.test_outputs_csv_path)
 
-            self.model.test_outputs_csv_path = "lightning_logs/%s/test_output_ood_%s.csv" % (self.args.log_name,fold_num)
+            self.model.test_outputs_csv_path = "lightning_logs/%s/test_output_ood_%s.csv" % (self.args.log_name,fold)
             trainer.test(model=self.model, dataloaders=ood_test_dataloader,ckpt_path=model_path)
             test_outputs_ood_csv_path_list.append(self.model.test_outputs_csv_path)
 
