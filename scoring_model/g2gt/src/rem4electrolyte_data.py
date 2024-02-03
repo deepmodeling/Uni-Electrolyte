774272fdba30f0f581d7f41e2fd4f1af602e20e7
ddd <542260219@qq.com>
2024-02-03 11:48:30 +0800

    g2g add model merging

--- scoring_model/g2gt/src/rem4electrolyte_data.py
+++ scoring_model/g2gt/src/rem4electrolyte_data.py
@@ -360,8 +360,8 @@ class Rem():
                 LearningRateMonitor(logging_interval='step'),
                 ModelCheckpoint(filename='{epoch}-{epoch_val_loss:.3f}', save_top_k=3, save_last=True,
                                 monitor="epoch_val_loss", mode='min', verbose=True, auto_insert_metric_name=True),
-                ModelCheckpoint(filename='best', save_top_k=1,
-                                monitor="epoch_val_loss", mode='min', verbose=True),
+                # ModelCheckpoint(filename='best', save_top_k=1,
+                #                 monitor="epoch_val_loss", mode='min', verbose=True),
             ],
             # limit_train_batches=20,
             # log_every_n_steps=10
@@ -425,15 +425,18 @@ class Rem():
             print('len(valid_dataloader)', len(valid_dataloader))
 
 
+
             trainer.fit(model=self.model, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader )
-            model_path="lightning_logs/%s/version_0/checkpoints/best.ckpt" % (self.args.log_name)
+            model_path="lightning_logs/%s/model_%s.ckpt" % (self.args.log_name,fold)
+            trainer.save_checkpoint(model_path)
 
+            trainer.max_epochs += self.args.epoch
 
-            self.model.test_outputs_csv_path = "lightning_logs/%s/test_output_iid_%s.csv" % (self.args.log_name, fold_idx)
+            self.model.test_outputs_csv_path = "lightning_logs/%s/test_output_iid_%s_%s.csv" % (self.args.log_name, fold,seed_int)
             trainer.test(model=self.model, dataloaders=iid_test_dataloader,ckpt_path=model_path)
             test_outputs_iid_csv_path_list.append(self.model.test_outputs_csv_path)
 
-            self.model.test_outputs_csv_path = "lightning_logs/%s/test_output_ood_%s.csv" % (self.args.log_name,fold_idx)
+            self.model.test_outputs_csv_path = "lightning_logs/%s/test_output_ood_%s_%s.csv" % (self.args.log_name,fold,seed_int)
             trainer.test(model=self.model, dataloaders=ood_test_dataloader,ckpt_path=model_path)
             test_outputs_ood_csv_path_list.append(self.model.test_outputs_csv_path)
 
