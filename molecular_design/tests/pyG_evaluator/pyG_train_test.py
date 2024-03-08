2b0f480b173c490a4c8792c51c55f3cd5c5268b9
ddd <542260219@qq.com>
2024-03-08 15:57:18 +0800

    LEFTnet test

--- molecular_design/tests/pyG_evaluator/pyG_train_test.py
+++ molecular_design/tests/pyG_evaluator/pyG_train_test.py
@@ -1,6 +1,7 @@
 import os
 
 import torch
+#export PYTHONPATH=$PYTHONPATH:/root/yinshiqiu/Uni-Electrolyte/molecular_design   to search the uni_electrolyte package
 from uni_electrolyte.evaluator.dataset import thuEMol
 from uni_electrolyte.evaluator.inference import pyG_inference_test, pyG_inference_train, pyG_inference_without_label
 from uni_electrolyte.evaluator.model.spatial import LEFTNet
