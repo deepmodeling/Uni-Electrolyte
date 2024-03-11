8317be5607032e73ae7860050e94a5d72b3dc9bd
ddd <542260219@qq.com>
2024-03-11 17:51:59 +0800

    add rareactdiff leftnet

--- molecular_design/uni_electrolyte/evaluator/model/spatial/__init__.py
+++ molecular_design/uni_electrolyte/evaluator/model/spatial/__init__.py
@@ -5,7 +5,6 @@ from .leftnet import LEFTNet
 from .painn import PaiNN
 from .schnet import SchNet
 from .so3net import SO3net
-from .leftnet_oareactdiff import LEFTNet as OA_REACTDIFF_LEFTNet
 
 __all__ = [
     'DimeNetPP',
@@ -15,5 +14,4 @@ __all__ = [
     'SO3net',
     'SchNet',
     'PaiNN',
-    "OA_REACTDIFF_LEFTNet"
 ]
