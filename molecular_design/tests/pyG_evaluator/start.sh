c1a492100bc260b6daf5b99ec42dbfd3acda5e3c
ddd <542260219@qq.com>
2024-03-18 18:44:08 +0800

    g2g+LEFTnet

--- molecular_design/tests/pyG_evaluator/start.sh
+++ molecular_design/tests/pyG_evaluator/start.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$PYTHONPATH:/root/yinshiqiu/Uni-Electrolyte/molecular_design:/root/yinshiqiu/Uni-Electrolyte/molecular_design/uni_electrolyte/evaluator/model/spatial
-python pyG_train_test.py $1
+python /root/yinshiqiu/Uni-Electrolyte/molecular_design/tests/pyG_evaluator/pyG_train_test.py $1
