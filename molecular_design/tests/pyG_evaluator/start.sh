51b39121da7ae4d78b6a85b6cce9a660664ab93a
ddd <542260219@qq.com>
2024-03-21 18:06:27 +0800

    g2g+LEFTnet+g2gnofreeze

--- molecular_design/tests/pyG_evaluator/start.sh
+++ molecular_design/tests/pyG_evaluator/start.sh
@@ -1,3 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:/root/yinshiqiu/Uni-Electrolyte/molecular_design:/root/yinshiqiu/Uni-Electrolyte/molecular_design/uni_electrolyte/evaluator/model/spatial
 #cd /root/yinshiqiu/Uni-Electrolyte/molecular_design/tests/pyG_evaluator/
+cp ./pyG_train_test.py /root/yinshiqiu/Uni-Electrolyte/molecular_design/tests/pyG_evaluator/pyG_train_test.py #for bohrium task 更灵活
 python /root/yinshiqiu/Uni-Electrolyte/molecular_design/tests/pyG_evaluator/pyG_train_test.py $1 $2 $3
