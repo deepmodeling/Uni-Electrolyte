b661c5b1d8cb7226c8ebd59c6215ddb4fd6f6c1b
ddd <542260219@qq.com>
2024-04-18 13:38:36 +0800

    workbench

--- workbench/controllers/predict_properties.py
+++ workbench/controllers/predict_properties.py
@@ -5,18 +5,53 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 from views.helper import render_section, render_secondary_section
 from views.predict_properties import options_view,screen_switch_view
+# @callback(
+#     Output("Predict Property And Screen","children"),
+#     [Input("Screen molecules from database","n_clicks")]
+# )
+# def show_screen_switch(n_clicks):
+#     print("dddddddddddddddddddddddddddddddddddddddddd")
+#     if n_clicks is not None:
+#         print("eeeeeeeeeeeeeeeeeeeeeeeeeee")
+#
+#         return screen_switch_view()
+
+
+
 @callback(
-    Output("Predict Property And Screen","children"),
-    [Input("Screen molecules from database","n_clicks")]
+    [Output("left sidebar", "sidebarChildren"),
+     Output("right sidebar","sidebarChildren"),
+     Output("Exploration Details","sidebarChildren"),
+     Output("Exploration Details","mainChildren"),
+     ],
+    [Input("Screen molecules from database", "n_clicks")]
 )
-def show_screen_switch(n_clicks):
-    print("dddddddddddddddddddddddddddddddddddddddddd")
+def show_predict_properties(n_clicks):
+
     if n_clicks is not None:
-        print("eeeeeeeeeeeeeeeeeeeeeeeeeee")
+        # 在这里编写你要输出的内容
+        print("ffffffffffffffffff")
 
-        return screen_switch_view()
+        right_sidebar= dbc.Row(
+                            render_secondary_section(
+                                "Configure Exploration", "", options_view()
+                            ),
+                            id="row-options-view",
+                            style={
+                                "height": "100%",
+                                "alignItems": "flexStart",
+                                "paddingRight": "2%",
+                                "paddingTop": "1rem",
+                            },
+                        ),
+                        # UserTrack.get_component(),
 
 
+
+        output_text = "菜单项被点击了！"
+        return right_sidebar,output_text,output_text,output_text
+    else:
+        return "","","",""
 
 
 @callback(
